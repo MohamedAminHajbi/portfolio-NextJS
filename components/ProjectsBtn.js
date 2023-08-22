eaba36fe81f58ba7bbcdc60e51e2e8253bef4aaa
Mohamed_Amin_Hajbi <aminhajbi116@gmail.com>
2023-08-22 17:28:26 +0100

    Update home page

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -5,9 +5,9 @@ import {HiArrowRight} from 'react-icons/hi2'
 const ProjectsBtn = () => {
   return (
     <div>
-      <Link href={'/work'} className="relative w-[185px] h-[185px] flex justify-center items-center">
-        <Image src={'/rounded-text.png'} height={148} width={141} alt="" className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"/>
-        <HiArrowRight className="absolute text-4xl"/>  
+      <Link href={'/work'} className="relative w-[115px] h-[115px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
+        <Image src={'/rounded-text.png'} height={100} width={94} alt="" className="animate-spin-slow w-full h-full max-w-[94px] max-h-[100px]"/>
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300"/>  
       </Link>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import ParticlesContainer from '../components/ParticlesContainer'
 import ProjectsBtn from '../components/ProjectsBtn'
 import Avatar from '../components/Avatar'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import { fadeIn } from '../variants'
 
 
@@ -10,14 +10,17 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradiant-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
-          <h1 className="h1">
+        <div className="text-center flex flex-col justify-center xl:text-left h-full container mx-auto xl:pb-0 pb-20">
+          <motion.h2 variants={fadeIn('down',0.2)} initial='hidden' animate='show' exit='hidden' className="h2">
             Transforming Ideas <br/> Into <span className="text-accent">Digital Reality</span>
-          </h1>
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </motion.h2>
+          <motion.p variants={fadeIn('down',0.3)} initial='hidden' animate='show' exit='hidden' className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</motion.p>
           <div className="flex justify-center xl:hidden">
             <ProjectsBtn/>
           </div>
+          <motion.div variants={fadeIn('down',0.4)} initial='hidden' animate='show' exit='hidden' className="hidden xl:flex xl:pb-10">
+            <ProjectsBtn/>
+          </motion.div>
         </div>
       </div>
     </div>
