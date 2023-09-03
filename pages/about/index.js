5e3de6a387271b0005c16ab8ec409953395dec8f
MohamedAminHajbi <aminhajbi116@gmail.com>
2023-09-03 16:52:27 +0100

    Text and icons

--- pages/about/index.js
+++ pages/about/index.js
@@ -90,7 +90,8 @@ const About = () => {
     </motion.div>
     <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
       <div className='flex-1 flex flex-col justify-center'>
-        Text
+        <h2 className='h2'>Captivating <span className='text-accent'>stories</span> birth magnificent designs.</h2>
+        <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>10 years ago, I began freelancing as a developer. Since then, I've done remote work for agencies, consulted for startups, collaborated on digital products for business and consumer use.</p>
       </div>
       <div className='flex flex-col w-full xl:max-w-[48%] h-[380px]'>
         <div className={'flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'}>
@@ -104,20 +105,24 @@ const About = () => {
             })
           }
         </div>
-        <div className='bg-pink-400/10 py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
+        <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
           {
             aboutData[index].info.map((item,itemIndex) => {
               return(
-                <div key={itemIndex}>
-                  <div>
+                <div key={itemIndex} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'>
+                  <div className='font-light mb-2 md:mb-0'>
                     {item.title}
                   </div>
                   <div className='hidden md:flex'>-</div>
                   <div>{item.stage}</div>
-                  <div>
+                  <div className='flex gap-x-4'>
                     {
-                      item.icons.map((icon,iconIndex) => {
-                        return (<div>{icon}</div>)
+                      item.icons?.map((icon,iconIndex) => {
+                        return (
+                        <div key={iconIndex} className='text-2xl text-white'>
+                          {icon}
+                        </div>
+                        )
                       })
                     }
                   </div>
