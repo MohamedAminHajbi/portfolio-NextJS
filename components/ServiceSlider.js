b93d94627972206c48df6fe6631c5e7e35f3ffb0
MohamedAminHajbi <aminhajbi116@gmail.com>
2023-09-12 18:00:05 +0100

    Services done

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,4 +1,4 @@
-import {RxCrop,RxPencil2,RxDesktop,RxReader,RxRocket} from 'react-icons/rx';
+import {RxCrop,RxPencil2,RxDesktop,RxReader,RxRocket, RxArrowTopRight} from 'react-icons/rx';
 import {SwiperSlide , Swiper} from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/free-mode'
@@ -42,16 +42,24 @@ const ServiceSlider = () => {
           slidesPerView: 1,
           spaceBetween: 15
         },
-        640: {
+        540: {
           slidesPerView: 3,
           spaceBetween: 15
         }
-      }} pagination={{clickable: true}} freeMode= {true} modules={{FreeMode, Pagination}} className='h-[240px] sm:h-[340px]'>
+      }} freeMode= {true} pagination={{clickable: true}} modules={[FreeMode, Pagination]} className='h-[240px] sm:h-[340px]'>
         {
           serviceData.map((item, index) => {
             return(
               <SwiperSlide key={index}>
-                <div>
+                <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300'>
+                  <div className='text-4xl text-accent mb-4'>{item.icon}</div>
+                  <div className='mb-8'>
+                    <div className='mb-2 text-lg'>{item.title}</div>
+                    <div className='max-w-[350px] leading-normal'>{item.description}</div>
+                  </div>
+                  <div className='text-3xl'>
+                    <RxArrowTopRight className='group-hover:rotate-45 group-hover:text-accent transition-all duration-300'/>
+                  </div>
                   
                 </div>
               </SwiperSlide>
--- pages/services/index.js
+++ pages/services/index.js
@@ -11,14 +11,17 @@ const Services = () => {
     <div className='container mx-auto'>
       <div className='flex flex-col xl:flex-row gap-x-8'>
         <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-          <h2 className='h2 xl:mt-8'>
+          <motion.h2 variants={fadeIn('down', 0.3)} initial='hidden' animate='show' exit='hidden' className='h2 xl:mt-8'>
             My services <span className='text-accent'>.</span>
-          </h2>
-          <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+          </motion.h2>
+          <motion.p variants={fadeIn('up', 0.4)} initial='hidden' animate='show' exit='hidden' className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
             Eu nisi Lorem ut voluptate et enim ad duis dolor ea aliquip. Excepteur nostrud nostrud eiusmod minim cillum esse id aute do. Dolore veniam nulla officia dolore sunt pariatur consequat. Esse laboris aliqua laboris ex ut est. Ullamco non aliqua labore minim eu ea et ea occaecat officia.
-          </p>
+          </motion.p>
         </div>
-        <ServiceSlider/>
+        <motion.div variants={fadeIn('left', 0.5)} initial='hidden' animate='show' exit='hidden' className='w-full xl:max-w-[65%]'>
+          <ServiceSlider/>
+        </motion.div>
+        
       </div>
     </div>
     <Bulb/>
