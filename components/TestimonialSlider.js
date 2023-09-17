09f2771cdc80d7b833b4f0612d1b227619e09e4f
MohamedAminHajbi <aminhajbi116@gmail.com>
2023-09-17 16:37:58 +0100

    Testimonials

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,3 +1,11 @@
+import {FaQuoteLeft} from 'react-icons/fa';
+import {SwiperSlide , Swiper} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+import { Pagination } from 'swiper';
+import Image from 'next/image';
+
 // testimonial data
 const testimonialData = [
   {
@@ -24,7 +32,42 @@ const testimonialData = [
 ];
 
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <>
+      <Swiper spaceBetween={10} pagination={{clickable: true}} modules={[ Pagination]} className='h-[440px] sm:h-[540px]'>
+        {
+          testimonialData.map((person, index) => {
+            return(
+              <SwiperSlide key={index}>
+                <div className=''>
+                  <div key={index}>
+                    <div className=''>
+                          <div className=''>
+                            Avatar
+                          </div>
+                          <div className=''>
+                            Name
+                          </div>
+                          <div className=''>
+                            Position
+                          </div>
+                    </div>
+                  </div>
+                  <div className='bg-pink-500/10 flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20'>
+                    <div className='mb-4'>
+                    <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0'/>
+                    </div>
+                    <div>{person.message}</div>
+                  </div>
+                </div>
+                
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </>
+  );
 };
 
 export default TestimonialSlider;
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,14 @@
+import TestimonialSlider from "../../components/TestimonialSlider";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center">
+      <div className="h2 mb-8 xl:mb-0">
+        <h2>What clients <span className="text-accent">say.</span></h2>
+        <div><TestimonialSlider/></div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
