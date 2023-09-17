b30ca843b134ded313057b3ccac3032e920d8a48
MohamedAminHajbi <aminhajbi116@gmail.com>
2023-09-17 15:32:10 +0100

    Work done

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -54,7 +54,7 @@ const workSlides = {
 
 
 
-const ServiceSlider = () => {
+const WorkSlider = () => {
   return (
     <>
       <Swiper spaceBetween={10} pagination={{clickable: true}} modules={[ Pagination]} className='h-[440px] sm:h-[540px]'>
@@ -66,10 +66,17 @@ const ServiceSlider = () => {
                   {
                   slide.images.map((image,index) => {
                     return(
-                      <div className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
+                      <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={index}>
                         <div className='flex items-center justify-center relative overflow-hidden'>
                           <Image src={image.path} width={500} height={300} alt=''/>
                           <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                          <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+                            <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+                            <div className='delay-100'>Live</div>
+                            <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>Project</div>
+                            <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight/></div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     )
@@ -86,5 +93,5 @@ const ServiceSlider = () => {
   );
 };
 
-export default ServiceSlider;
+export default WorkSlider;
 
