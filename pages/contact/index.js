003a879c7f7184bbc5dc12ee90a3ac339c801786
MohamedAminHajbi <aminhajbi116@gmail.com>
2023-09-20 14:18:16 +0100

    Contact done

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,32 @@
+import Circles from '../../components/Circles'
+import {BsArrowRight} from 'react-icons/bs'
+import {motion} from 'framer-motion'
+import {fadeIn} from '../../variants'
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='h-full bg-primary/30'>
+      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
+        <div className='flex flex-col w-full max-w-[700px]'>
+          <h2 className='h2 text-center mb-12'>
+            Let's <span className='text-accent'>connect.</span>
+          </h2>
+          <form className='flex-1 flex flex-col gap-6 w-full mx-auto'>
+            <div className='flex gap-x-6 w-full'>
+              <input type='text' placeholder='name' className='input'/>
+              <input type='text' placeholder='email' className='input'/>
+            </div>
+            <input type='text' placeholder='subject' className='input'/>
+            <textarea placeholder='Write your message here...' className='textarea'></textarea>
+            <button className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
+              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>Let's talk</span>
+              <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]'/>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
